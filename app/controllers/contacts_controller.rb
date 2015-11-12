cbff625745ff1b86a65224b877700ef5e05c7358
Samantha Morco <morkotiks@gmail.com>
2015-11-11 19:58:44 -0800

    now I can use a form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,4 +3,14 @@ class ContactsController < ApplicationController
   def contacts
     @contacts = ContactInfo.all
   end
+
+  def add_new
+
+  end
+
+  def contact_added
+    ContactInfo.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone_number])
+    @contacts = ContactInfo.all
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   get '/contacts', to: 'contacts#contacts'
+  get '/add_new', to: 'contacts#add_new'
+  post '/contact_added', to: 'contacts#contact_added'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
